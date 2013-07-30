50616de21261c48d25913defe3d304615c93982f
dcmonger <unexrogh@gmail.com>
2013-07-30 15:02:24 -0700

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 TestApp::Application.configure do
+    config.force_sll = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
